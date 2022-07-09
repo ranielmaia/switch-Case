bedf357a86db123946fb8f89055dc2f7b2241349
Raniel Maia <raniel_maia@hotmail.com>
2022-07-08 21:36:51 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let temporada = "Primavera";
+
+switch (temporada) {
+    case temporada = "Verão":
+        console.log("Opte por roupas leves e beba água!");
+        break;
+    case temporada = "Outono":
+        console.log("Melhor levar um casaco.")
+        break;
+    case temporada = "Inverno":
+        console.log("Tem que usar 3 peças de roupa.")
+        break;
+    case temporada = "Primavera":
+        console.log("Só uma camiseta serve.")
+        break;
+    default:
+        console.log("Isso não é uma estação.")
+}
